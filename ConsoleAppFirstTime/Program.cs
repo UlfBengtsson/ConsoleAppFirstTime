cc35ccce43ef0355fa8fad8f15506b7203a0de32
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2020-09-07 10:19:28 +0200

    edit massage

--- ConsoleAppFirstTime/Program.cs
+++ ConsoleAppFirstTime/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppFirstTime
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World, i like to code!");
         }
     }
 }
